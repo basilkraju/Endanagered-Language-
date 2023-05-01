c9501948a14100aba51e6b81a906897cbc28b9f8
Basil K Raju <basilkraju1998@gmail.com>
2023-05-01 18:29:03 +0530

    upto two websites

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home,name ='home'),
+    path('gradio_app/', views.gradio_app, name='gradio_app'),
+    path('predict/',views.predict, name = 'predict')
+    
    
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,41 @@
+#from django.shortcuts import render
 from django.shortcuts import render
+import gradio as gr
+import os
+import requests
+# import torch
+# from transformers import AutoProcessor, WhisperForConditionalGeneration
+#from datasets import load_dataset
+# import librosa
+import os
+# import pandas as pd
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+# from myapp.models import WhisperModel
+from django.http import HttpResponse
 
-# Create your views here.
+API_URL = "https://api-inference.huggingface.co/models/basilkr/Whisper_Malasar_50"
+headers = {"Authorization": "Bearer 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"}
+
 def home(request):
-    return render(request, 'home.html',{})
+    return render(request, 'ho.html',{})
+def gradio_app(request):
+    return render(request, 'gradio_app.html')
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+def predict(request):
+    if request.method == 'POST':
+        audio_file = request.FILES['audio_file'].file.read()
+
+        try:
+            output = query(audio_file)
+            return JsonResponse(output, safe=False)
+        except UnicodeDecodeError:
+            return HttpResponse("Error: Invalid audio file", status=400)
+    return render(request, 'predict.html')
